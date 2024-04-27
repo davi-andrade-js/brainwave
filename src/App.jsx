44da925a6ea739d05153dedbb15c5f25fb4fe982
Davi <dv.andrade04@gmail.com>
2024-04-27 13:48:02 -0300

    implement header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
-import Button from "./components/Button.jsx";
+import ButtonGradient from '../public/assets/svg/ButtonGradient.jsx';
+// import Button from './components/Button.jsx';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-teal-400 underline">
-        Hello world!
-      </h1>
+      {/* <h1 className="text-3xl font-bold text-teal-400 underline">Hello world!</h1> */}
+      <Header />
 
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login"></Button>
-      </div>
+      {/* <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10">Hello World</Button>
+      </div> */}
 
       <ButtonGradient />
     </>
