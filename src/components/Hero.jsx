322d3e184941f53c3aa885866a959e26ebf09942
Davi <dv.andrade04@gmail.com>
2024-04-30 16:06:58 -0300

    finish hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import {curve, heroBackground, robot} from "/public/assets";
 import Button from "./Button";
 import Section from "./Section";
-import {BackgroundCircles, Gradient} from "./design/Hero";
+import {BackgroundCircles, BottomLine, Gradient} from "./design/Hero";
 import {heroIcons} from "../constants";
 import {ScrollParallax} from "react-just-parallax";
 import {useRef} from "react";
+import Generating from "./Generating";
+import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -61,6 +64,11 @@ const Hero = () => {
                   alt="AI"
                 />
 
+                <Generating
+                  className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem]
+                md:-translate-x-1/2"
+                />
+
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul
                     className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur 
@@ -73,10 +81,17 @@ const Hero = () => {
                     ))}
                   </ul>
                 </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code generation"
+                  />
+                </ScrollParallax>
               </div>
             </div>
-            <Gradient />
           </div>
+          <Gradient />
           <div
             className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:-w-[138%] 
             lg:-top-[104%]"
@@ -86,7 +101,11 @@ const Hero = () => {
 
           <BackgroundCircles />
         </div>
+
+        <CompanyLogos className="relative x-10 mt-20 lg:block" />
       </div>
+
+      <BottomLine />
     </Section>
   );
 };
